cf3c1cb9d06e81360d5828e73b4c184d76560931
Juanpam <jpkratos@gmail.com>
2017-09-15 11:45:47 -0500

    Initial map class added to the project

---
+++ map.py
@@ -0,0 +1,12 @@
+"""
+This module implements a Map class used to represent in a logic way the map in Pakman
+"""
+
+class Map():
+    """
+    This class implements using a matrix the Pakman map. It's used to generate the optimal path for each
+    ghost and to draw the map object's in the screen acordingly
+    """
+
+    def __init__(self):
+        pass
